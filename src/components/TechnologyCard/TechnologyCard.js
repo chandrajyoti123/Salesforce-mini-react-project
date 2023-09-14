bdf20ceca7375ccf247def745be493ad268954fb
mansidethe <mansidethe18@gmail.com>
2023-09-14 11:41:10 +0530

    initial commit

---
+++ src/components/TechnologyCard/TechnologyCard.js
@@ -0,0 +1,28 @@
+import './TechnologyCard.css'
+
+export default function TechnologyCard()
+{
+    return(
+        <h1 className='text'>Our technology helps people create better jobs, <br/>lives, companies, and communities.</h1>
+    )
+}
+export function CardOne()
+{
+    return(
+        <h1>$1.5B</h1>
+    )
+}
+
+export function CardTwo()
+{
+    return(
+        <h1>FORTUNE</h1>
+    )
+}
+
+export function CardThree()
+{
+    return(
+        <h1>FORTUNE</h1>
+    )
+}
