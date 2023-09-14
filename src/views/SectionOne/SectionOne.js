7e280bfbe996eb75b60848759eab6c7d2316abb8
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-09-14 17:06:42 +0530

    section one added

--- src/views/SectionOne/SectionOne.js
+++ src/views/SectionOne/SectionOne.js
@@ -1,5 +1,23 @@
+import Button, {DarkButton} from '../../components/Button/Button'
+import backgroundimage from './backgroundimage2.webp'
+import './SectionOne.css'
+
 export default function SectionOne(){
     return(
-        <h1>hello</h1>
+        <div className='sectionOne'>
+           <div className='sectionOneOfOne'>
+           <div className='slogen'>AI + Data + CRM = more <br/> sales and happier <br/> customers. </div>
+          <div className='text'>Discover trusted AI that helps you connect with your customers in <br/> a whole new way.</div>
+          <div className='buttonGroup'>
+            <DarkButton text="Start Free Trial" color={"BlueButton"}/> 
+         <div className='buttonRight'>
+         <Button text={"Watch Demo"}/>
+         </div>
+          </div>
+           </div>
+           <div className=' sectionOneOfTwo'>
+            <img className='sectionOneImage' src={backgroundimage}/>
+           </div>
+        </div>
     )
 }
