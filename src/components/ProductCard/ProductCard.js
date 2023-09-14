b1d9e39d993e488dc7c6ed1e21df5fe0ce58687c
kashishInamdar <kashishinamdar2004@gmail.com>
2023-09-14 16:20:58 +0530

    Update Buttons

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,3 @@
+import './ProductCard.css'
+
+export 
--- src/views/SectionTwo/SectionTwo.js
+++ src/views/SectionTwo/SectionTwo.js
@@ -1,5 +1,14 @@
+import { DarkButton } from "../../components/Button/Button"
+import Button  from "../../components/Button/Button"
+
 export default function SectionTwo(){
     return(
+        <>
         <h1>hello</h1>
+        <Button text="Demo Viwe" />
+<br/>
+<br/>
+        <DarkButton text="Demo Viwe" color={"GreenButton"}/>
+        </>
     )
 }
