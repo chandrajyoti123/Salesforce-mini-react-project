53039006e5091bbeea4630ff0d6d802d1e2b351b
mansidethe <mansidethe18@gmail.com>
2023-09-15 11:43:16 +0530

    added cards

---
+++ src/components/CompanyLogoCard/CompanyLogoCard.js
@@ -0,0 +1,14 @@
+import './CompanyLogoCard.css'
+
+export default function CompanyLogoCard({img}){
+    return(
+       
+<div className='card'>
+    
+    <img src={img} className='image-logo'/>
+</div>
+
+          
+   
+    )
+}
