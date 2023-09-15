fd1133297c2209f43a793c69b62f77a2cfc5779b
kashishInamdar <kashishinamdar2004@gmail.com>
2023-09-15 13:00:20 +0530

    Complice Product Card section

--- src/views/SectionTwo/SectionTwo.js
+++ src/views/SectionTwo/SectionTwo.js
@@ -1,4 +1,4 @@
-import { DarkButton } from "../../components/Button/Button"
+import './SectionTwo.css'
 import Button from "../../components/Button/Button"
 import ProductCard from "../../components/ProductCard/ProductCard"
 // -------- Import Images --------- 
@@ -6,32 +6,48 @@ import small from './smallBusiness.webp'
 import seles from './salesCloud.webp'
 import sercice from './serviceCloud.webp'
 import marketing from './marketingCloud.webp'
+import bgImage from './productBG.webp'
 
 export default function SectionTwo() {
     return (
         <>
+        <div >
             <div className="container ">
-                <div className="row">
+                <div className="row ">
+                    <div className="col ">
+                       <h1 className='heading'> Learn what Salesforce products can do for you.</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="container ">
+                <div className="row ">
                     <div className="col">
-                        <ProductCard ImgURL={small} Title={'Small Businss'} conteint={'Sales, service, and email outreach tools in a single app.'} link={'Try for free'}  />
+                        <ProductCard ImgURL={small} Title={'Small Businss'} conteint={'Sales, service, and email outreach tools in a single app.'} link={'Try for free'} />
                     </div>
                     <div className="col">
-                        <ProductCard ImgURL={seles } Title={'Sales Cloud'} conteint={'Close more deals and speed up throgh growth with the #1 CRM.'} link={'watcj demo'}/>
+                        <ProductCard ImgURL={seles} Title={'Sales Cloud'} conteint={'Close more deals and speed up throgh growth with the #1 CRM.'} link={'watch demo'} />
                     </div>
                     <div className="col">
-                        <ProductCard ImgURL={sercice} conteint={'Make customers happy faster and build loyalty with Service Cloud.'} Title={'Service Cloud'} link={'Watch demo'}/>
+                        <ProductCard ImgURL={sercice} conteint={'Make customers happy faster and build loyalty with Service Cloud.'} Title={'Service Cloud'} link={'Watch demo'} />
                     </div>
                     <div className="col">
                         <ProductCard ImgURL={marketing} Title={'Marketing Cloud'} conteint={'Build customer relationships for life with data-first digital marketing.'} link={'Watch demo'} />
                     </div>
                 </div>
             </div>
-
-
-            <Button text="Demo Viwe" />
             <br />
             <br />
-            <DarkButton text="Demo Viwe" color={"GreenButton"} />
+            <div className="container ">
+                <div className="row ">
+                    <div className="col ">
+                        <Button text="Viwe all our product" />
+                    </div>
+                </div>
+            </div>
+            <img src={bgImage} alt='img' width='100%' className='bg-image' />
+
+        </div>
+
         </>
     )
 }
