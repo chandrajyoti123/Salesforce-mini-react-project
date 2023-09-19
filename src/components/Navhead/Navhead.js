1a2ddf651dc8b20bd657fe6f02e40b14762be744
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-09-19 10:24:03 +0530

    added some changes in pages

--- src/components/Navhead/Navhead.js
+++ src/components/Navhead/Navhead.js
@@ -3,7 +3,7 @@ export default function Navhead(){
     return(
         <>
         <div className="navhead">
-            <div className="asd">  <span className="text4"> dreamforce</span> </div>
+            <div className="asd">  <span className="textminor"> dreamforce</span> </div>
             <div className="cvb"> <span className="text5"> Dreamforce: the asia Pacific Edition </span> </div>
             <div  className="bnm"><a href="#" className="alink"> Watch on demand</a> </div>
         
