865d724942af3f60855d1960571c96d361998a1d
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-09-30 13:18:07 +0530

    made some new changes

--- src/views/Customers/Customers.js
+++ src/views/Customers/Customers.js
@@ -22,9 +22,10 @@ export default function Customers(){
         <Navbar/>
        
         
-        <div className="phonebottom"><PhoneBottom head={"Hear our story"} btntext={"Learn about us"}/></div>
+        
        <div className="sideBar">
         <SideBarCust/>
+        <div className="phonebottom"><PhoneBottom head={"Hear our story"} btntext={"Learn about us"}/></div>
         {/* Question? We can help */}
        
         <div className='sidebarright'>
